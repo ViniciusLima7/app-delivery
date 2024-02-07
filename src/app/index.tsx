5ce648b92c245c01874e278117f9789608327550
Vinicius Lima <marcos.vf@samsung.com>
2024-02-07 19:45:05 -0300

    fix: type SectionList

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
 import { Product } from "@/components/product";
 import { useCartStore } from "@/stores/cart-store";
-import { CATEGORIES, MENU } from "@/utils/data/products";
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products";
 import { Link } from "expo-router";
 import { useRef, useState } from "react";
 import { FlatList, SectionList, Text, View } from "react-native";
@@ -14,7 +14,7 @@ export default function Home() {
     (totalQuantity, product) => totalQuantity + product.quantity,
     0
   );
-  const sectionListRef = useRef<SectionList>(null);
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory);
 
