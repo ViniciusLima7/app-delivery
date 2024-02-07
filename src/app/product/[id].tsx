1aff0d07b6545660bcf0dc7e9a0f5cfd58700d4a
Vinicius Lima <marcos.vf@samsung.com>
2024-02-06 21:39:59 -0300

    feat: add component product details

---
+++ src/app/product/[id].tsx
@@ -0,0 +1,33 @@
+import { PRODUCTS } from "@/utils/data/products";
+import { formatCurrency } from "@/utils/functions/format-currency";
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+export default function Product() {
+  const { id } = useLocalSearchParams();
+  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  console.log("🚀 ~ Product ~ product:", product);
+  return (
+    <View className="flex-1">
+      <Image
+        source={product.cover}
+        className="w-full h-52"
+        resizeMode="cover"
+      />
+      <View className="p-5 mt-8 flex-1">
+        <Text className="text-lime-400 text-2xl font-heading my-2">
+          {formatCurrency(product.price)}
+        </Text>
+        <Text className="text-slate-400  font-body text-base leading-6 mb-6">
+          {product.description}
+        </Text>
+        {product.ingredients.map((ingredient) => (
+          <Text key={ingredient} className="text-slate-400 text-base leading-6">
+            {"\u2022"} {ingredient}
+          </Text>
+        ))}
+      </View>
+    </View>
+  );
+}
