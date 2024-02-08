05ef607c81fb96fc506ddbe4f81673d8ac6ca20a
Vinicius Lima <marcos.vf@samsung.com>
2024-02-07 22:50:48 -0300

    feat: add async storage e save data in device client

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import * as cartInMemory from "./helpers/cart-in-memory";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export type ProductCartProps = ProductProps & {
   quantity: number;
 };
@@ -11,16 +13,23 @@ type StateProps = {
   remove: (productId: string) => void;
 };
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
-  add(product) {
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    }));
-  },
-  remove(productId: string) {
-    set((state) => ({
-      products: cartInMemory.remove(state.products, productId),
-    }));
-  },
-}));
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
+
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+    }),
+    {
+      name: "nlw-expert-delivery:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
