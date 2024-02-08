2e079e440ef522d17852ebc2bc43138f770c67fe
Vinicius Lima <marcos.vf@samsung.com>
2024-02-07 22:01:05 -0300

    feat: add buttons finish order and back in homepage

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/button";
 import { Header } from "@/components/header";
+import { Input } from "@/components/input";
+import { LinkButton } from "@/components/link-button";
 import { Product } from "@/components/product";
 import { useCartStore } from "@/stores/cart-store";
 import { formatCurrency } from "@/utils/functions/format-currency";
+import { Feather } from "@expo/vector-icons";
 import React from "react";
 import { ScrollView, Text, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function Cart() {
   const cartStore = useCartStore();
@@ -16,24 +21,43 @@ export default function Cart() {
   );
   return (
     <View className="flex-1 pt-8">
-      <ScrollView>
-        <Header title="Seu Carrinho" />
-        {cartStore.products.length > 0 ? (
+      <Header title="Seu Carrinho" />
+      <KeyboardAwareScrollView
+        showsVerticalScrollIndicator={false}
+        extraHeight={100}
+      >
+        <ScrollView>
           <View className="p-5 flex-1">
-            {cartStore.products.map((product) => (
-              <Product key={product.id} data={product} />
-            ))}
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700">
+                {cartStore.products.map((product) => (
+                  <Product key={product.id} data={product} />
+                ))}
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my8">
+                Seu Carrinho está vazio
+              </Text>
+            )}
+            <View className="flex-row gap-2 items-center mt-5 mb-4">
+              <Text className="text-white text-xl font-subtitle">Total:</Text>
+              <Text className="text-lime-400 text-2xl font-heading">
+                {total}
+              </Text>
+            </View>
+            <Input placeholder="Informe o endereçod e entrega como rua, bairro, CEP, número" />
           </View>
-        ) : (
-          <Text className="font-body text-slate-400 text-center my8">
-            Seu Carrinho está vazio
-          </Text>
-        )}
-        <View className="flex-row gap-2 items-center mt-5 mb-4 ml-4">
-          <Text className="text-white text-xl font-subtitle">Total:</Text>
-          <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </KeyboardAwareScrollView>
+      <View className="p-5 gap-5">
+        <Button>
+          <Button.Text>Enviar Pedido</Button.Text>
+          <Button.Icon>
+            <Feather name="arrow-right-circle" size={20} />
+          </Button.Icon>
+        </Button>
+        <LinkButton title="Voltar ao Cardápio" href="/" />
+      </View>
     </View>
   );
 }
